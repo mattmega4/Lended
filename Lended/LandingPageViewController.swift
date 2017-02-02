235438b80b7da16016b512fc8176fd66ebda83c4
Matthew Singleton <mattmega4@gmail.com>
2017-02-02 00:49:23 -0800

    First commit on branch

--- Lended/LandingPageViewController.swift
+++ Lended/LandingPageViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class LandingPageViewController: UIViewController {
+  
+  
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var contentView: UIView!
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +21,5 @@ class LandingPageViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
