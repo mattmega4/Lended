cf66cfb7adf4a34e4ce6ba364ad8ad2e251022d0
Matthew Singleton <mattmega4@gmail.com>
2017-02-12 23:14:10 -0800

    Created all the logic between switching between create account and log in, also created logic for the action button (sign in, continue, create account) including all the textfield targeted delegate methods, also switched the keyboard types based on what step you are on, added new images

--- Lended/EntryPageViewController.swift
+++ Lended/EntryPageViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseDatabase
 
 class EntryPageViewController: UIViewController {
   
@@ -24,12 +25,22 @@ class EntryPageViewController: UIViewController {
   @IBOutlet weak var addLabel: UILabel!
   
   @IBOutlet weak var bottomContainerView: UIView!
-  @IBOutlet weak var emailTextField: UITextField!
-  @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var bottomTextFieldOne: UITextField!
+  @IBOutlet weak var bottomTextFieldTwo: UITextField!
   @IBOutlet weak var signInOrUpButtonContainerView: UIView!
   @IBOutlet weak var signInOrUpButton: UIButton!
   
-
+  var topFieldIsSatisfied: Bool?
+  var bottomFieldIsSatisfied: Bool?
+  var nextButtonRequirementsHaveBeenMet: Bool?
+  
+  var userStateIsOnSignIn: Bool?
+  var createUserStepOneFinished: Bool?
+  
+  var newUserEmail: String?
+  var newUserPassword: String?
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -43,8 +54,10 @@ class EntryPageViewController: UIViewController {
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     
+    setStatusToDefaultWhichIsSignIn()
+    checkIfBothSignInRequirementsAreMet()
   }
-
+  
   
   // MARK: Add Keyboard Targets
   
@@ -56,10 +69,165 @@ class EntryPageViewController: UIViewController {
   }
   
   
+  // MARK: Switch Logic For Sign In or Create Button in Right Container View
+  
+  func rightContaienrStateSwitcher() {
+    if userStateIsOnSignIn == true {
+      setStatusToSignUpStageOne()
+    } else if userStateIsOnSignIn == false {
+      setStatusToDefaultWhichIsSignIn()
+    }
+  }
   
   
+  // MARK: Switch Logic For Sign In or Create Button in Bottom Container View
+  
+  func bottomContainerStateSwitcher() {
+    if userStateIsOnSignIn == true {
+      signUserIn()
+    } else if userStateIsOnSignIn == false && createUserStepOneFinished == false {
+      newUserEmail = bottomTextFieldOne.text ?? ""
+      setStatusToSignUpStateTwo()
+    } else if userStateIsOnSignIn == false && createUserStepOneFinished == true {
+      createNewUser()
+    }
+  }
   
   
+  // MARK: Logic For State Switching
+  
+  func setStatusToDefaultWhichIsSignIn() {
+    createUserStepOneFinished = false
+    userStateIsOnSignIn = true
+    topFieldIsSatisfied = false
+    bottomFieldIsSatisfied = false
+    nextButtonRequirementsHaveBeenMet = false
+    addButton.setImage(UIImage.init(named: "Plus.png"), for: UIControlState())
+    addLabel.text = "Create Account"
+    bottomTextFieldTwo.isHidden = false
+    bottomTextFieldTwo.isEnabled = true
+    bottomTextFieldOne.text = ""
+    bottomTextFieldTwo.text = ""
+    bottomTextFieldOne.placeholder = "Email"
+    bottomTextFieldTwo.placeholder = "Password"
+    bottomTextFieldOne.keyboardType = .emailAddress
+    bottomTextFieldTwo.keyboardType = .default
+    bottomTextFieldOne.addTarget(self, action: #selector(checkIfTopTextFieldIsSatisfiedForLogin(textField:)), for: .editingChanged)
+    bottomTextFieldTwo.addTarget(self, action: #selector(checkIfBottomTextFieldIsSatisfiedForLogin(textField:)), for: .editingChanged)
+    signInOrUpButton.setTitle("SIGN IN", for: UIControlState())
+  }
+  
+  
+  func setStatusToSignUpStageOne() {
+    checkIfBothCreateRequirementsAreMet()
+    userStateIsOnSignIn = false
+    topFieldIsSatisfied = false
+    bottomFieldIsSatisfied = false
+    nextButtonRequirementsHaveBeenMet = false
+    addButton.setImage(UIImage.init(named: "Lock.png"), for: UIControlState())
+    addLabel.text = "I Have An Account"
+    bottomTextFieldOne.text = ""
+    bottomTextFieldTwo.text = ""
+    bottomTextFieldTwo.isHidden = true
+    bottomTextFieldTwo.isEnabled = false
+    bottomTextFieldOne.placeholder = "Email"
+    bottomTextFieldOne.keyboardType = .emailAddress
+    bottomTextFieldOne.addTarget(self, action: #selector(checkIfTopTextFieldIsSatisfiedForCreatePartOne(textField:)), for: .editingChanged)
+    signInOrUpButton.setTitle("CONTINUE", for: UIControlState())
+    
+  }
+  
+  
+  func setStatusToSignUpStateTwo() {
+    checkIfBothCreateRequirementsAreMet()
+    createUserStepOneFinished = true
+    userStateIsOnSignIn = false
+    topFieldIsSatisfied = false
+    bottomFieldIsSatisfied = false
+    nextButtonRequirementsHaveBeenMet = false
+    bottomTextFieldTwo.isHidden = false
+    bottomTextFieldTwo.isEnabled = true
+    bottomTextFieldOne.text = ""
+    bottomTextFieldTwo.text = ""
+    bottomTextFieldOne.placeholder = "Password"
+    bottomTextFieldTwo.placeholder = "Confirm Password"
+    bottomTextFieldOne.keyboardType = .default
+    bottomTextFieldTwo.keyboardType = .default
+    bottomTextFieldOne.addTarget(self, action: #selector(checkIfTopTextFieldIsSatisfiedForCreatePartTwo(textField:)), for: .editingChanged)
+    bottomTextFieldTwo.addTarget(self, action: #selector(checkIfBottomTextFieldIsSatisfiedForCreatePartTwo(textField:)), for: .editingChanged)
+    signInOrUpButton.setTitle("CREATE ACCOUNT", for: UIControlState())
+    
+  }
+  
+  
+  // MARK: Continue Condition Checking Logic
+  
+  func checkIfBothSignInRequirementsAreMet() {
+    if topFieldIsSatisfied == true && bottomFieldIsSatisfied == true {
+      signInOrUpButton.isEnabled = true
+      signInOrUpButton.isHidden = false
+      signInOrUpButtonContainerView.isHidden = false
+    } else {
+      signInOrUpButton.isEnabled = false
+      signInOrUpButton.isHidden = true
+      signInOrUpButtonContainerView.isHidden = true
+    }
+  }
+  
+  
+  func checkIfTopContinueRequirementIsMet() {
+    if topFieldIsSatisfied == true {
+      signInOrUpButton.isEnabled = true
+      signInOrUpButton.isHidden = false
+      signInOrUpButtonContainerView.isHidden = false
+    } else {
+      signInOrUpButton.isEnabled = false
+      signInOrUpButton.isHidden = true
+      signInOrUpButtonContainerView.isHidden = true
+    }
+  }
+  
+  
+  func checkIfBothCreateRequirementsAreMet() {
+    if topFieldIsSatisfied == true && bottomFieldIsSatisfied == true && bottomTextFieldOne.text == bottomTextFieldTwo.text {
+      signInOrUpButton.isEnabled = true
+      signInOrUpButton.isHidden = false
+      signInOrUpButtonContainerView.isHidden = false
+    } else {
+      signInOrUpButton.isEnabled = false
+      signInOrUpButton.isHidden = true
+      signInOrUpButtonContainerView.isHidden = true
+    }
+  }
+  
+  
+  // TODO: SIgn In
+  
+  func signUserIn() {
+    
+  }
+  
+  
+  // TODO: Create User
+  
+  func createNewUser() {
+    
+  }
+  
+  
+  // TODO: IB Actions
+  
+  @IBAction func questionMarkButtonTapped(_ sender: UIButton) {
+    // Lost Password
+  }
+  
+  @IBAction func addButtonTapped(_ sender: UIButton) {
+    rightContaienrStateSwitcher()
+  }
+  
+  @IBAction func signInOrUpTapped(_ sender: UIButton) {
+    bottomContainerStateSwitcher()
+  }
   
   
   // MARK: Keyboard Methods
@@ -96,7 +264,8 @@ class EntryPageViewController: UIViewController {
     textField.resignFirstResponder()
     return false
   }
-
+  
+  
   
 } // End of EntryPageViewController
 
@@ -106,13 +275,75 @@ extension EntryPageViewController: UITextFieldDelegate {
   
   func bottomTextFieldDelegateAndAutoCorrectAndPlaceholderColorSetup() {
     var bottomTextFields: [UITextField] = []
-    bottomTextFields+=[emailTextField, passwordTextField]
+    bottomTextFields+=[bottomTextFieldOne, bottomTextFieldTwo]
     let placeHolderLightColor = UIColor.lightText
     for fields in bottomTextFields {
       fields.autocorrectionType = .no
       fields.delegate = self
       fields.attributedPlaceholder = NSAttributedString(string: fields.placeholder!, attributes: [NSForegroundColorAttributeName : placeHolderLightColor])
     }
+  }
+  
+  
+  // TODO: Bottom Text Field Targets
+  
+  func checkIfTopTextFieldIsSatisfiedForLogin(textField: UITextField) {
+    if textField == bottomTextFieldOne {
+      if textField.text?.validateEmail() == true {
+        topFieldIsSatisfied = true
+      } else {
+        topFieldIsSatisfied = false
+      }
+    }
+    checkIfBothSignInRequirementsAreMet()
+  }
+  
+  
+  func checkIfBottomTextFieldIsSatisfiedForLogin(textField: UITextField) {
+    if textField == bottomTextFieldTwo {
+      if textField.text?.isEmpty == true {
+        bottomFieldIsSatisfied = false
+      } else {
+        bottomFieldIsSatisfied = true
+      }
+    }
+    checkIfBothSignInRequirementsAreMet()
+  }
+  
+  
+  func checkIfTopTextFieldIsSatisfiedForCreatePartOne(textField: UITextField) {
+    if textField == bottomTextFieldOne {
+      if textField.text?.validateEmail() == true {
+        topFieldIsSatisfied = true
+      } else {
+        topFieldIsSatisfied = false
+      }
+    }
+    checkIfTopContinueRequirementIsMet()
+  }
+  
+  
+  func checkIfTopTextFieldIsSatisfiedForCreatePartTwo(textField: UITextField) {
+    if textField == bottomTextFieldOne {
+      if textField.text?.isEmpty == true {
+        topFieldIsSatisfied = false
+      } else {
+        topFieldIsSatisfied = true
+      }
+    }
+    checkIfBothCreateRequirementsAreMet()
+  }
+  
+  
+  func checkIfBottomTextFieldIsSatisfiedForCreatePartTwo(textField: UITextField) {
+    if textField == bottomTextFieldTwo {
+      if textField.text?.isEmpty == true {
+        bottomFieldIsSatisfied = false
+      } else {
+        bottomFieldIsSatisfied = true
+      }
+    }
+    checkIfBothCreateRequirementsAreMet()
   }
   
   
--- Lended/General+Extensions.swift
+++ Lended/General+Extensions.swift
@@ -15,3 +15,11 @@ extension UIButton {
     clipsToBounds = true
   }
 }
+
+
+extension String {
+  func validateEmail() -> Bool {
+    let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+    return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: self)
+  }
+}
