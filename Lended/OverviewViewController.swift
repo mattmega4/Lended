5477a2be29adc4f6a1db1f3ce4eaa39ee6191770
Matthew Singleton <mattmega4@gmail.com>
2017-02-13 00:39:34 -0800

    added tbc and 2 top level vc both in a nav c

---
+++ Lended/OverviewViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OverviewViewController.swift
+//  Lended
+//
+//  Created by Matthew Singleton on 2/13/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class OverviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
