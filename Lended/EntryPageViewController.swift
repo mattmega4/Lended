670614ebcaa45a3527eed8cf6c92ac698cc76f20
Matthew Singleton <mattmega4@gmail.com>
2017-02-12 18:10:30 -0800

    Removed autocorrect, added keyboard methods, added Pragma Marks

--- Lended/EntryPageViewController.swift
+++ Lended/EntryPageViewController.swift
@@ -27,12 +27,12 @@ class EntryPageViewController: UIViewController {
   @IBOutlet weak var signInOrUpButtonContainerView: UIView!
   @IBOutlet weak var signInOrUpButton: UIButton!
   
-  var bottomTextFields: [UITextField] = []
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    bottomTextFieldArrayDelegateAndPlaceholderSetup()
+    bottomTextFieldDelegateAndAutoCorrectAndPlaceholderColorSetup()
+    keyboardMethods()
     
   }
   
@@ -42,21 +42,70 @@ class EntryPageViewController: UIViewController {
   }
 
   
+  // MARK: Add Keyboard Targets
+  
+  func keyboardMethods() {
+    let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(EntryPageViewController.dismissKeyboard))
+    view.addGestureRecognizer(tap)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: nil)
+  }
   
   
   
   
   
   
+  
+  
+  // MARK: Keyboard Methods
+  
+  func keyboardWillShow(notification:NSNotification) {
+    var userInfo = notification.userInfo!
+    var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+    keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+    var contentInset: UIEdgeInsets = self.scrollView.contentInset
+    contentInset.bottom = keyboardFrame.size.height + 30
+    self.scrollView.contentInset = contentInset
+  }
+  
+  
+  func keyboardWillHide(notification:NSNotification) {
+    let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+    self.scrollView.contentInset = contentInset
+  }
+  
+  
+  func dismissKeyboard() {
+    view.endEditing(true)
+  }
+  
+  
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    self.view.endEditing(true)
+    return false
+  }
+  
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    self.view.endEditing(true)
+    textField.resignFirstResponder()
+    return false
+  }
+
   
 } // End of EntryPageViewController
 
 extension EntryPageViewController: UITextFieldDelegate {
   
-  func bottomTextFieldArrayDelegateAndPlaceholderSetup() {
+  // MARK: Add Delegate, Remove AutoCorrect, and Placeholder Color to Bottom TextFields
+  
+  func bottomTextFieldDelegateAndAutoCorrectAndPlaceholderColorSetup() {
+    var bottomTextFields: [UITextField] = []
     bottomTextFields+=[emailTextField, passwordTextField]
     let placeHolderLightColor = UIColor.lightText
     for fields in bottomTextFields {
+      fields.autocorrectionType = .no
       fields.delegate = self
       fields.attributedPlaceholder = NSAttributedString(string: fields.placeholder!, attributes: [NSForegroundColorAttributeName : placeHolderLightColor])
     }
