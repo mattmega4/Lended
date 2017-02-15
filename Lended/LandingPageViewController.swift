3401f605d3681dbfb135431c9c06a77b5c5a40a6
Matthew Singleton <mattmega4@gmail.com>
2017-02-15 01:51:56 -0800

    Added 'Add Account' VC

--- Lended/LandingPageViewController.swift
+++ Lended/LandingPageViewController.swift
@@ -7,28 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class LandingPageViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        setNavBar()
-    }
-
+  
+  // IB Outlets
+  
+  let ref = FIRDatabase.database().reference()
+  let user = FIRAuth.auth()?.currentUser
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setNavBar()
+  }
+  
   
   // MARK: Nav Bar & View Design
   
   func setNavBar() {
     title = "Accounts"
-    navigationController?.navigationBar.barTintColor = UIColor(red: 229.0/255.0, green: 112.0/255.0, blue: 85.0/255.0, alpha: 1.0) // Blue
+    navigationController?.navigationBar.barTintColor = UIColor(red: 229.0/255.0, green: 112.0/255.0, blue: 85.0/255.0, alpha: 1.0)
     UINavigationBar.appearance().tintColor = .white
     UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
     navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white,
                                                                NSFontAttributeName: UIFont(name: "Futura",
                                                                                            size: 20)!]
   }
-
+  
   
   
   
